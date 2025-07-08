562a132ad0ee241f2ae93ca56e1b97b7fd6c7554
Hosea Nganga <hoseanganga27@gmail.com>
2025-07-08 13:21:39 +0300

    Update webpack.config.js

--- apps/manga-market-be/webpack.config.js
+++ apps/manga-market-be/webpack.config.js
@@ -4,7 +4,7 @@ const { join } = require('path');
 module.exports = {
   output: {
     //path: join(__dirname, 'dist'),
-    path: join(__dirname, '../../../dist/apps/manga-market-be'),
+     path: join(__dirname, '..', '..', '..', 'dist', 'apps', 'manga-market-be'),
   },
   plugins: [
     new NxAppWebpackPlugin({
